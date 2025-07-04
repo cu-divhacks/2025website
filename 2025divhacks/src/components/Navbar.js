0c19797b0ab41f011c01d80757b63e348ba04496
al1cezhan9 <alice.az.zhang@gmail.com>
2025-07-04 16:18:35 -0400

    fixed sponsors and judges header, added dropdown, haven't finish linking

--- 2025divhacks/src/components/Navbar.js
+++ 2025divhacks/src/components/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../App.css';
 
 const Navbar = () => {
   const location = useLocation();
+  const [isHomeDropdownOpen, setIsHomeDropdownOpen] = useState(false);
+  const timeoutRef = useRef(null);
 
   const scrollToSection = (sectionId) => {
     // If we're not on the home page, navigate there first
@@ -24,33 +26,74 @@ const Navbar = () => {
     <nav className="navbar navbar-custom">
       <div className="nav-container-custom">
         <div className="navbar-nav">
-          <Link 
-            className={`nav-link-custom ${location.pathname === '/' ? 'active' : ''}`} 
-            to="/"
+          <div 
+            className="nav-item dropdown"
+            onMouseEnter={() => {
+              if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+              }
+              setIsHomeDropdownOpen(true);
+            }}
+            onMouseLeave={() => {
+              timeoutRef.current = setTimeout(() => {
+                setIsHomeDropdownOpen(false);
+              }, 100);
+            }}
           >
-            HOME
-          </Link>
-          <button 
-            className="nav-link-custom" 
-            onClick={() => scrollToSection('about')}
-            style={{ background: 'none', border: 'none', color: 'inherit' }}
-          >
-            ABOUT
-          </button>
-          <button 
-            className="nav-link-custom" 
-            onClick={() => scrollToSection('tracks')}
-            style={{ background: 'none', border: 'none', color: 'inherit' }}
-          >
-            TRACKS
-          </button>
-          <button 
-            className="nav-link-custom" 
-            onClick={() => scrollToSection('faq')}
-            style={{ background: 'none', border: 'none', color: 'inherit' }}
-          >
-            FAQs
-          </button>
+            <Link 
+              className={`nav-link-custom ${location.pathname === '/' ? 'active' : ''}`} 
+              to="/"
+            >
+              HOME
+            </Link>
+            {isHomeDropdownOpen && (
+              <div 
+                className="dropdown-menu"
+                onMouseEnter={() => {
+                  if (timeoutRef.current) {
+                    clearTimeout(timeoutRef.current);
+                  }
+                  setIsHomeDropdownOpen(true);
+                }}
+                onMouseLeave={() => {
+                  timeoutRef.current = setTimeout(() => {
+                    setIsHomeDropdownOpen(false);
+                  }, 100);
+                }}
+              >
+                <button 
+                  className="dropdown-item" 
+                  onClick={() => scrollToSection('about')}
+                >
+                  About
+                </button>
+                <button 
+                  className="dropdown-item" 
+                  onClick={() => scrollToSection('tracks')}
+                >
+                  Tracks
+                </button>
+                <button 
+                  className="dropdown-item" 
+                  onClick={() => scrollToSection('judges-speakers')}
+                >
+                  Judges & Speakers
+                </button>
+                <button 
+                  className="dropdown-item" 
+                  onClick={() => scrollToSection('sponsors')}
+                >
+                  Sponsors
+                </button>
+                <button 
+                  className="dropdown-item" 
+                  onClick={() => scrollToSection('faq')}
+                >
+                  FAQ
+                </button>
+              </div>
+            )}
+          </div>
           <Link 
             className={`nav-link-custom ${location.pathname === '/schedule' ? 'active' : ''}`} 
             to="/schedule"
