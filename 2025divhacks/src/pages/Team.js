e58f0a676833c8c5673c71fc3a18d9731413dc55
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2025-07-22 12:03:52 -0400

    edits

--- 2025divhacks/src/pages/Team.js
+++ 2025divhacks/src/pages/Team.js
@@ -7,6 +7,8 @@ import adneen_talib from '../assets/adneen_talib.png';
 import leah_kim from '../assets/leah_kim.png';
 import isabella_lu from '../assets/isabella_lu.png';
 import grace_xu from '../assets/grace_xu.png';
+import alice_zhang from '../assets/alice.jpeg';
+import angel_wu from '../assets/angel.jpeg';
 import teamHeader from '../assets/team_header.png';
 
 const Team = () => {
@@ -52,8 +54,29 @@ const Team = () => {
       role: 'Technical Dev Lead',
       email: 'gx2168@columbia.edu',
       image: grace_xu
-    }
-  ];
+    }] 
+    const techTeam = [
+      {
+        name: 'Grace Xu',
+        role: 'Technical Dev Lead',
+        email: 'gx2168@columbia.edu',
+        image: grace_xu
+      }, 
+      {
+        name: 'Alice Zhang',
+        role: 'Tech Dev Team',
+        email: 'az2880@columbia.edu',
+        image: alice_zhang
+      },
+      {
+        name: 'Angel Wu',
+        role: 'Tech Dev Team',
+        email: 'aw3631@columbia.edu',
+        image: angel_wu
+      },
+    ];
+  
+  
 
   return (
     <div style={{ position: 'relative', minHeight: '100vh' }}>
@@ -77,6 +100,20 @@ const Team = () => {
         
         <div className="team-grid">
           {teamMembers.map((member, index) => (
+            <div className="team-member" key={index}>
+              <img src={member.image} alt={member.name} />
+              <div className="team-name">{member.name}</div>
+              <div className="team-role">{member.role}</div>
+              <a className="team-email" href={`mailto:${member.email}`}>
+                {member.email}
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <h2 style={{ color: '#fff', fontSize: '3.5vw', marginBottom: '3vw', marginTop: '8vw' }}>Web Development Team</h2>
+        <div className="team-grid">
+          {techTeam.map((member, index) => (
             <div className="team-member" key={index}>
               <img src={member.image} alt={member.name} />
               <div className="team-name">{member.name}</div>
